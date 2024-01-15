4f0e4c9d40bce2187279880880353830a92bee29
nitin devatraj <nitindevatraj@protonmail.com>
2024-01-15 20:00:49 +0530

    add initial pages, routes, components and global css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/homepage/Homepage";
+import Pricing from "./pages/pricing/Pricing";
+import Product from "./pages/product/Product";
+import Login from "./pages/login/Login";
+import AppLayout from "./pages/app-layout/AppLayout";
+import PageNotFound from "./pages/page-not-found/PageNotFound";
+
 export default function App() {
-  return <div>world wise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/app-layout/AppLayout.jsx
@@ -0,0 +1,10 @@
+import AppNav from "../../components/app-nav/AppNav";
+
+export default function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <h1>AppLayout</h1>
+    </div>
+  );
+}
